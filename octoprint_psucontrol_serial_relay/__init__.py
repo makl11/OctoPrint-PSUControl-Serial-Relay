148360e67d0f6e3357b1c4ac95b62363fc19aefb
Max Klingelhoeffer <makl11@users.noreply.github.com>
2023-04-18 22:15:19 +0200

    refactor reconnect logic and use custom dataclass for config

--- octoprint_psucontrol_serial_relay/__init__.py
+++ octoprint_psucontrol_serial_relay/__init__.py
@@ -1,8 +1,43 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-import octoprint.plugin
+from dataclasses import InitVar, dataclass, fields
+
 from serial import Serial
+
+import octoprint.plugin
+import octoprint.settings
+
+
+@dataclass
+class PluginConfig:
+    owner: InitVar[octoprint.plugin.SettingsPlugin]
+
+    def reload(self):
+        for f in fields(self):
+            if f.type == str:
+                v = self.owner._settings.get([f.name])
+            elif f.type == int:
+                v = self.owner._settings.get_int([f.name])
+            elif f.type == float:
+                v = self.owner._settings.get_float([f.name])
+            elif f.type == bool:
+                v = self.owner._settings.get_boolean([f.name])
+            else:
+                raise TypeError(f"Fields of type {f.type} are not supported in a Config")
+            self[f.name] = v
+
+    def __post_init__(self, owner):
+        self.owner = owner
+
+    def __iter__(self):
+        return ((f.name, getattr(self, f.name, None)) for f in fields(self))
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
 
 
 class PSUControl_Serial_Relay(
@@ -11,52 +46,39 @@ class PSUControl_Serial_Relay(
     octoprint.plugin.TemplatePlugin,
     octoprint.plugin.SettingsPlugin,
 ):
-    DEFAULT_CONFIG = {"port": "", "baudrate": -1}
-    config: dict
+    @dataclass
+    class Config(PluginConfig):
+        port: str = "Disabled"
+        baudrate: int = 9600
+
+    config: Config
     serial_relay: Serial | None
 
     def __init__(self):
-        self.config = self.DEFAULT_CONFIG
+        self.config = self.Config(owner=self)
         self.serial_relay = None
 
     def __del__(self):
         if self.serial_relay is not None and self.serial_relay.is_open:
             self.serial_relay.close()
 
-    def reload_config(self):
-        for k, v in self.DEFAULT_CONFIG.items():
-            if type(v) == str:
-                v = self._settings.get([k])
-            elif type(v) == int:
-                v = self._settings.get_int([k])
-            elif type(v) == float:
-                v = self._settings.get_float([k])
-            elif type(v) == bool:
-                v = self._settings.get_boolean([k])
-
-            self.config[k] = v
-            self._logger.debug("{}: {}".format(k, v))
-
-    def reconnect(self):
-        if self.serial_relay is not None and self.serial_relay.is_open:
-            self.serial_relay.close()
-        self.serial_relay = Serial(self.config["port"], self.config["baudrate"])
-        self.serial_relay.open()
-
     def ensure_connection(self):
-        if (
-            isinstance(self.serial_relay, Serial)
-            and (
-                self.serial_relay.port != self.config["port"]
-                or self.serial_relay.baudrate != self.config["baudrate"]
-                or not self.serial_relay.is_open
-            )
-        ) or (
-            self.serial_relay is None
-            and self.config["port"] != ""
-            and self.config["baudrate"] > 0
-        ):
-            self.reconnect()
+        if self.serial_relay is None:
+            if self.config.port != "Disabled" and self.config.baudrate > 0:
+                self.serial_relay = Serial(self.config.port, self.config.baudrate)
+        else:
+            if not self.serial_relay.is_open:
+                self.serial_relay.open()
+
+            if self.serial_relay.baudrate != self.config.baudrate:
+                self.serial_relay.baudrate = self.config.baudrate
+
+            if self.serial_relay.port != self.config.port:
+                if self.config.port == "Disabled":
+                    self.serial_relay.close()
+                    self.serial_relay = None
+                else:
+                    self.serial_relay.port = self.config.port
 
     # PSUControl
     def on_startup(self, host, port):
@@ -91,15 +113,18 @@ class PSUControl_Serial_Relay(
 
     # Settings
     def on_settings_initialized(self):
-        self.reload_config()
+        self.config.reload()
         self.ensure_connection()
 
     def get_settings_defaults(self):
-        return self.DEFAULT_CONFIG
+        return {k: v for k, v in self.Config(owner=self)}
 
     def on_settings_save(self, data):
+        # TODO: Remove this hack by using a custom viewmodel
+        if "port" in data and data["port"] is None:
+            data["port"] = "Disabled"
         octoprint.plugin.SettingsPlugin.on_settings_save(self, data)
-        self.reload_config()
+        self.config.reload()
         self.ensure_connection()
 
     def get_settings_version(self):
@@ -122,6 +147,7 @@ class PSUControl_Serial_Relay(
                 "type": "github_release",
                 "user": "makl11",
                 "repo": "OctoPrint-PSUControl-Serial-Relay",
+                "prerelease": "True",
                 "current": self._plugin_version,
                 # update method: pip
                 "pip": "https://github.com/makl11/OctoPrint-PSUControl-Serial-Relay/archive/{target_version}.zip",
